95f2846121c3de76698c6ccdde5feac6a137fbb6
divyadillibabu <86547135+divyadillibabu@users.noreply.github.com>
2021-06-27 12:11:19 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import Fooder from "./Fooder";
+import Info from "./Info";
+
+function App() {
+  return(
+    <div>
+      <Header />
+      <Fooder />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      </div>
+  );
+}
+export default App;
---
+++ Fooder.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Fooder(){
+  return (
+ <footer>
+    <p>Copyright By ShapeAI @ {new Date().getFullYear()}</p>
+  </footer>
+  
+
+  );
+}
+export default  Fooder;
+
+
